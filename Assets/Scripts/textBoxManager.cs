8f6eefa191b2d635b0d303331abea828bf7e0c46
Clara Alder <quixoticduck@gmail.com>
2016-09-09 01:24:19 +0100

    first commit

---
+++ Assets/Scripts/textBoxManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class textBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+
+	public Text theText;
+
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+//	public PlayerController player;
+
+	public bool isActive;
+	
+	// Use this for initialization
+	void Start () 
+	{
+//		player = FindObjectOfType<PlayerController>();
+
+		if(textFile != null)
+			
+		{
+			// \n represents a line break (whenever our text file has an enter pressed)
+			textLines = (textFile.text.Split ('\n'));
+		}
+
+		if (endAtLine == 0)
+		{
+			endAtLine = textLines.Length - 1;
+		}
+
+		if (isActive) {
+			EnabletextBox();
+		}
+
+		else {
+			DisabletextBox();
+		}
+
+	}
+
+
+	void Update()
+	{
+		if (!isActive)
+		{
+			return;
+		}
+
+		theText.text = textLines[currentLine];
+
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			currentLine +=1;
+		}
+
+
+		if(currentLine > endAtLine) 
+		{
+			DisabletextBox();
+		}
+
+	}
+
+	public void EnabletextBox()
+	{
+		textBox.SetActive(true);
+	}
+
+	public void DisabletextBox()
+	{
+		textBox.SetActive(false);
+	}
+}
